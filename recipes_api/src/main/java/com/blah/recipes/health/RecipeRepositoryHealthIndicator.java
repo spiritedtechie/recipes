c2d1cff5f4b5a1d787756dea25518d3084445f35
Vijay Patel <veejspeej@gmail.com>
2018-12-23 15:52:41 +0000

    Adds a custom health indicator for the recipe repository.

---
+++ recipes_api/src/main/java/com/blah/recipes/health/RecipeRepositoryHealthIndicator.java
@@ -0,0 +1,38 @@
+package com.blah.recipes.health;
+
+import com.blah.recipes.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class RecipeRepositoryHealthIndicator implements HealthIndicator {
+
+    private static final String DEFAULT_RECIPE_FETCHED_KEY = "defaultRecipeFetched";
+
+    private Function<Boolean, Health.Builder> booleanToHealth = (Boolean isUp) -> isUp ? Health.up() : Health.down();
+
+    private RecipeRepository recipeRepository;
+
+    @Autowired
+    public RecipeRepositoryHealthIndicator(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Health health() {
+        try {
+            var a = recipeRepository.findByName("Boiled Eggs");
+
+            boolean found = a.isEmpty();
+            return booleanToHealth.apply(found)
+                    .withDetail(DEFAULT_RECIPE_FETCHED_KEY, found)
+                    .build();
+        } catch (Exception e) {
+            return Health.down(e).build();
+        }
+    }
+}
