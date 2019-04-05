1ca6605d95b5217489bfd82bf5833fee7cca11fa
Vijay Patel <veejspeej@gmail.com>
2019-04-05 14:32:23 +0100

    Add OPTIONS as allowed method for CORS - needed by javascript side to prefetch options.

--- recipes_api/src/main/java/com/blah/config/WebConfig.java
+++ recipes_api/src/main/java/com/blah/config/WebConfig.java
@@ -9,6 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3001");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:3001");
     }
 }
